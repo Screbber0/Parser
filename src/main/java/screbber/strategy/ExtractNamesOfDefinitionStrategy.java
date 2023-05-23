f0a7b617ed5c91c7acc69568a4edc1a3f4c1a320
Screbber <sachesnokov_1@edu.hse.ru>
2023-05-23 19:01:59 +0300

    fix comments

--- src/main/java/screbber/strategy/ExtractNamesOfDefinitionStrategy.java
+++ src/main/java/screbber/strategy/ExtractNamesOfDefinitionStrategy.java
@@ -20,28 +20,11 @@ public class ExtractNamesOfDefinitionStrategy extends SimpleTextExtractionStrate
 
         boolean isItalic = (renderInfo.getFont().getPostscriptFontName().contains("AJEUSD+DSFreeSet-BoldItalic"));
 
-//        if (renderInfo.getFont().getPostscriptFontName().contains("AJEUSD+DSFreeSet-BoldItalic")) {
-//            System.out.println(renderInfo.getText());
-//        }
         return isItalic;
     }
 
     @Override
     public void renderText(TextRenderInfo renderInfo) {
-        //System.out.println(renderInfo.getText() + ": ");
-        for (String[] strings : renderInfo.getFont().getFamilyFontName()) {
-            for (String font : strings) {
-                // System.out.print(font + ", ");
-            }
-        }
-
-        // System.out.print(renderInfo.getFont().toString());
-        // System.out.print(renderInfo.getFillColor() == null ? "null" : renderInfo.getFillColor().getRGB() + " ");
-        // System.out.println(BaseColor.BLACK.getRGB() + " " + BaseColor.BLUE.getRGB());
-
-//        System.out.println();
-//        System.out.println("Стиль : " + renderInfo.getFont().getPostscriptFontName());
-//        System.out.println("-----------------------------");
         boolean isItalic = isDefinition(renderInfo);
         if (isItalic) {
             super.renderText(renderInfo);
